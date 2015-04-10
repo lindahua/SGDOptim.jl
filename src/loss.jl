5af28b11cc0355d9d100d47e63cfb17cac157f67
Dahua Lin <lindahua@gmail.com>
2015-04-10 18:30:06 +0800

    add logistic loss and hinge loss

--- src/loss.jl
+++ src/loss.jl
@@ -7,8 +7,10 @@ abstract ScalarLoss <: Loss
 abstract MultinomialLoss <: Loss
 
 
-# squared loss
-
+## Squared loss (for linear regression)
+#
+#   loss(θ, x, y) := (1/2) * (θ'x - y)^2
+#
 type SqrLoss <: ScalarLoss
 end
 
@@ -26,4 +28,84 @@ function call(::SqrLoss, g::DenseVector, θ::DenseVector, x::DenseMatrix, y::Den
     r = x'θ - y
     A_mul_B!(g, x, r)
     return 0.5 * sumabs2(r)
+end
+
+
+## Hinge loss (for SVM)
+#
+#   loss(θ, x, y) := max(1 - y * θ'x, 0)
+#
+type HingeLoss <: ScalarLoss
+end
+
+hingeloss! = HingeLoss()
+
+# for a single sample
+function call(::HingeLoss, g::DenseVector, θ::DenseVector, x::DenseVector, y::Real)
+    r = dot(θ, x) * y
+    if r >= one(r)
+        fill!(g, 0)
+    else
+        scale!(g, y, x)
+    end
+    max(1.0 - r, 0.0)
+end
+
+# for a sample batch
+function call(::HingeLoss, g::DenseVector, θ::DenseVector, x::DenseMatrix, y::DenseVector)
+    u = x'θ
+    fill!(g, 0)
+    v = zero(u)
+    for i = 1:length(u)
+        uy = u[i] * y[i]
+        if uy < one(uy)
+            axpy!(y[i], x, g)
+            v += (one(uy) - uy)
+        end
+    end
+    return v
+end
+
+
+## Logistic loss (for logistic regression)
+#
+#   loss(θ, x, y) := log(1 + exp(-y * θ'x))
+#
+type LogisticLoss <: ScalarLoss
+end
+
+logisticloss! = LogisticLoss()
+
+# for a single sample
+
+# computes (log(1 + exp(-x)), exp(-x) / (1 + exp(-x)))
+# in a numerically stable way
+#
+function _logistic_deriv(x::Real)
+    if x >= zero(x)
+        e = exp(-x)
+        (log1p(e), e / (one(e) + e))
+    else
+        e = exp(x)
+        (log1p(e) - x, one(e) / (one(e) + e))
+    end
+end
+
+function call(::LogisticLoss, g::DenseVector, θ::DenseVector, x::DenseVector, y::Real)
+    r = dot(θ, x) * y
+    v, dv = _logistic_deriv(r)
+    scale!(g, -y * dv, x)
+    return v
+end
+
+function call(::LogisticLoss, g::DenseVector, θ::DenseVector, x::DenseMatrix, y::DenseVector)
+    u = x'θ
+    v = 0.0
+    for i = 1:length(u)
+        vi, u[i] = _logistic_deriv(y[i] * u[i])
+        v += vi
+        u[i] *= (-y[i])
+    end
+    A_mul_B!(g, X, u)
+    return v
 end
--- test/loss.jl
+++ test/loss.jl
@@ -1,10 +1,41 @@
 using SGD
 using Base.Test
 
+
+function safe_loss_and_grad(lfun!, θ::Vector, X::Matrix, y::Vector)
+    n = size(X, 2)
+    v = 0.0
+    g = zeros(size(X,1))
+    gi = zeros(size(X,1))
+    for i = 1:n
+        vi = lfun!(gi, θ, X[:,i], y[i])
+        v += vi
+        g += gi
+    end
+    return (v, g)
+end
+
+
+
 θ = [1.0, 2.0, 3.0]
 x = [4.0, 3.0, 2.0]
-g = zeros(3)
+n = 100
+g = zeros(length(θ))
+
+# Squared loss
 
 v = sqrloss!(g, θ, x, 13.0)
 @test v == 4.5
 @test g == 3.0 * x
+
+v = sqrloss!(g, θ, -x, -12.0)
+@test v == 8.0
+@test g == -4.0 * (-x)
+
+X = randn(length(θ), n)
+y = X'θ + 0.3 * randn(n)
+
+vr, gr = safe_loss_and_grad(sqrloss!, θ, X, y)
+v = sqrloss!(g, θ, X, y)
+@test_approx_eq v vr
+@test_approx_eq g gr
